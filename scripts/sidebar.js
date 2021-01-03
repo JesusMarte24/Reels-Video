2cc3ef82e7a58f259330813f5c62cd6dcb6fcb46
Jesus Marte <jesusmarte2430@gmail.com>
2021-01-03 11:49:50 -0500

    Typo with sign button has been fixed and a bug with close sidebar menu has been fixed

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,9 +1,10 @@
 let htmlElements = {
   header__sidebar: document.getElementById("header__sidebar"),
-  sidebar__button: document.querySelector(".sidebar__button"),
+  sidebar__button: document.getElementById("sidebar__button"),
 };
 
 htmlElements.sidebar__button.addEventListener("click", () => {
   htmlElements.header__sidebar.classList.toggle("header__container--2");
   htmlElements.sidebar__button.classList.toggle("sidebar__button--close");
+  htmlElements.sidebar__button.classList.toggle("sidebar__button");
 });
