80f5316638947ccf85eade1049c65e49968d0e51
Jesus Marte <jesusmarte2430@gmail.com>
2021-01-06 17:57:06 -0500

    tiny changed in header section and the html to main section has been added

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,16 +1,19 @@
 let htmlElements = {
+  header: document.getElementById("header"),
   header__sidebar: document.getElementById("header__sidebar"),
   sidebar__button: document.getElementById("sidebar__button"),
   sidebar__buttonClose: document.getElementById("sidebar__button--close"),
 };
 
 htmlElements.sidebar__button.addEventListener("click", () => {
+  htmlElements.header.style.backgroundColor = "rgb(27, 28, 36)";
   htmlElements.header__sidebar.classList.toggle("header__container--2");
   htmlElements.sidebar__buttonClose.style.display = "inline-block";
   htmlElements.sidebar__button.style.display = "none";
 });
 
 htmlElements.sidebar__buttonClose.addEventListener("click", () => {
+  htmlElements.header.style.backgroundColor = "transparent";
   htmlElements.header__sidebar.classList.toggle("header__container--2");
   htmlElements.sidebar__button.style.display = "inline-block";
   htmlElements.sidebar__buttonClose.style.display = "none";
